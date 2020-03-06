aeb7c636ecda6891b6bd868359bdac9ac0265902
miyamotomasato <xmasatox0530x@gmail.com>
2020-03-06 15:50:28 +0900

    add urls.py

--- hack_u_sojo/urls.py
+++ hack_u_sojo/urls.py
@@ -3,5 +3,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('', include('sport_update.urls')),
+    path('select/', include('sport_update.urls')),
     path('admin/', admin.site.urls),
 ]
